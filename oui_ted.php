05f8e8993091a9ddcf5db4dc41d8f16fe3e5a4cb
Nicolas Morand <contact@nicolasmorand.com>
2018-08-20 14:42:29 +0200

    Updates the Provider class namespace

--- oui_ted.php
+++ oui_ted.php
@@ -31,9 +31,9 @@
 
 namespace Oui;
 
-if (class_exists('Oui\Provider')) {
+if (class_exists('Oui\Player\Provider')) {
 
-    class Ted extends Provider
+    class Ted extends Player\Provider
     {
         protected static $srcBase = '//embed.ted.com/';
         protected static $srcGlue = array('talks/', '/', '/');
